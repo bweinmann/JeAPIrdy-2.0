b5c6722c33c947ee0cc1fb494feb78df0b16b2b6
bweinmann <bsweinmann@gmail.com>
2022-04-11 13:28:55 -0400

    reducer initiated

---
+++ src/Reducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    settings: {
+        loading: false,
+        question_category: ``,
+        question_difficulty: ``,
+        question_type: ``,
+        question_number: 50,
+    }
+}
+
+export default Reducer
