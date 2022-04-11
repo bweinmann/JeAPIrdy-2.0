c5501536d881140f8c2bff75dee907647c1f6148
bweinmann <bsweinmann@gmail.com>
2022-04-11 12:06:45 -0400

    api url, json payload and api data storage initiated

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Settings() {
+
+    //storing API data
+    const [settings, setSettings] = useState(null);
+
+    useEffect(() => {
+        const apiURL = `https://opentdb.com/api_category.php`;
+    
+        fetch(apiURL)
+            .then((res) => res.json())
+            .then((res) => {
+                setSettings(res.trivia_categories);
+            });
+    }, [setSettings]);
+    
   return (
     <div>Settings</div>
   )
