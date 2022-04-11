406215781d9b008346718e09bb963a2ac38710b2
bweinmann <bsweinmann@gmail.com>
2022-04-11 15:32:42 -0400

    implemented decoding solution for API response

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
 import { useState, useEffect, useSelector, useDispatch } from 'react-redux'
 
+const decoded = function (html) {
+  const content = document.createElement('content')
+  content.innerHTML = html
+  return content.value
+}
+
 export default function Question() {
 
   //retrieve data from store
   const [settings, setSettings] = useState([])
   const [selected, setSelected] = useState(false)
   const [correct, setCorrect] = useState(null)
+  
   const score = useSelector(state => state.score)
   const questions = useSelector(state => state.questions)
-  const index = useSelector(state => state.index)
+  
+  const encoded = useSelector(state => state.questions)
 
+  useEffect(() => {
+  const decodedQuestions = encoded.map(q => {
+    return {
+      ...q,
+      question: decoded(q.question),
+      correct_answer: decoded(q.correct_answer),
+      incorrect_answer: q.incorrect_answer.map(a => decoded(a))
+    }
+  })
+  setQuestions(decodedQuestions)
+  }, [encoded])
+
+  const index = useSelector(state => state.index)
   const dispatch = useDispatch()
+
+  const question = question[index]
+  const answer = question.correct
 
   const getNum = max => {
     return Math.floor(Math.random()*Math.floor(max));
   }
-
-  const question = question[index]
-  const answer = question.correct
 
   //returns right and wrong answers together in a random position
   useEffect(() => {
