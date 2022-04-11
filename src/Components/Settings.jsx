c2e3bd65d25acb61a28d27e80b98e08a68573d33
bweinmann <bsweinmann@gmail.com>
2022-04-11 12:16:52 -0400

    options now selectable for settings

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -4,6 +4,8 @@ export default function Settings() {
 
     //storing API data
     const [settings, setSettings] = useState(null);
+    //second hook for category
+    const [category, setCategory] = useState("");
 
     useEffect(() => {
         const apiURL = `https://opentdb.com/api_category.php`;
@@ -14,8 +16,24 @@ export default function Settings() {
                 setSettings(res.trivia_categories);
             });
     }, [setSettings]);
+    //when the category is chosen, event occurs
+    const handleCategory = e => {
+        setCategory(e.target.value)
+    }
     
   return (
-    <div>Settings</div>
-  )
+    <div>
+        <div>
+            <h2>Choose a Category:</h2>
+            <select value={category} onChange={handleCategory}>
+            <option>All</option>
+            {settings && settings.map((setting) => (
+                <option value={setting} key={setting.id}>
+                    {setting.name}
+                </option>
+            ))}
+            </select>
+        </div>
+    </div>
+  );
 }
